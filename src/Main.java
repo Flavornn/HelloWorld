c7d43dd4a379db12f6b7fd1706138450af37c7ad
Arnas <arnasparaf@gmail.com>
2024-03-13 10:48:43 +0900

    Added things

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double area, radius;
+
+        radius = 33;
+
+        area = radius * radius * 3.1415;
+        System.out.println("Area is " + area);
+    }
+}
